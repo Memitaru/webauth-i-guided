2d1cb4b4df7bd7ca2c7a7f406cbec01c33c33771
Memitaru <amandatwah@gmail.com>
2019-06-04 19:45:39 -0700

    Password hashes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -17,6 +18,9 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
