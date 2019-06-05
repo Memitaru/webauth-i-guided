e028809cc909be4c1a1d1ccef6b8f6d694fc9f69
Memitaru <amandatwah@gmail.com>
2019-06-04 20:30:36 -0700

    Done

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  if (!user.username || !user.password){
+    return res.status(500).json({message: "Need username and password"})
+  }
+
+  if (user.password.length < 8){
+    return res.status(400).json({message: "Password too short"})
+  }
+
   const hash = bcrypt.hashSync(user.password, 14);
   user.password = hash;
 
